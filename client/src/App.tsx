3501cb1fc3c6e30cc7639679364b7387f4f2e081
Keitoto <keitasekihara@gmail.com>
2023-05-09 17:08:40 +0900

    ✨ Added Apollo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,47 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Header from '@/components/Header';
+import HomePage from '@/pages/HomePage';
+import ProjectDetailPage from '@/pages/ProjectDetailPage';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache,
+});
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <div className="container">hello</div>
-    </>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects/:id" element={<ProjectDetailPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
 
